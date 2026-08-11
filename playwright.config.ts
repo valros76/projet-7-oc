d10b96cd51b10f7766c5d8dc6ad7c5579ce166ce
Valérian Dufrène <valros76@gmail.com>
2026-08-11 14:21:11 +0200

    Fix : Update playwright config to fix error into dev command

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig<ConfigOptions>({
   },
 
   webServer: {
-    command: 'bun run build && bun run start',
+    command: 'bun run build && bun run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 180 * 1000, // On laisse 3 minutes max pour le démarrage de Nuxt
